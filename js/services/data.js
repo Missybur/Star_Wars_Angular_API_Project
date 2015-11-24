0b684c2ce9674f5421a4e52992a89c1e4a50b983
Missy Bur <missybur@gmail.com>
2015-11-24 09:15:21 -0800

    Unminify project

--- js/services/data.js
+++ js/services/data.js
@@ -1,1 +1,15 @@
-"use strict";var app=angular.module("services",[]);app.service("getAndStoreCharacter",["$http",function($http){var characterNames=[];this.getCharacter=function(url){console.log("");$http.get(url).then(function(data){characterNames.push({charUrl:data.data.url,charName:data.data.name});console.log(characterNames)})};this.characterNames=characterNames}]);
+"use strict";
+
+var app = angular.module('services', [])
+
+app.service('getAndStoreCharacter', ['$http', function($http){
+  var characterNames = [];
+  this.getCharacter = function(url){
+    console.log('')
+    $http.get(url).then(function(data){
+      characterNames.push({charUrl : data.data.url , charName : data.data.name})
+      console.log(characterNames)
+    })
+  };
+  this.characterNames = characterNames;
+}]);
