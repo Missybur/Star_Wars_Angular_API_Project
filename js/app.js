53e54f77a574f4406da5fd92f9730094a730ceec
Missy Bur <missybur@gmail.com>
2015-11-24 08:16:55 -0800

    Add webpack

--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
+"use strict";
+
+module.exports = "It works from app.js"
+
 angular.module("myapp", ["ui.router"]);
 
- // var myapp = angular.module('myapp', ["ui.router"])
-    // myapp.controller("ResidentCtrl", function($scope, $http, $stateParams) {
-    //     $http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(resp => {
-    //         $scope.character = resp.data;
-    //     });
-    // })
-    // myapp.controller("PlanetCtrl", function($scope, $http) {
-    //     $scope.planets = [];
-    //     $http.get("http://swapi.co/api/planets/?format=json").then(resp => {
-    //         $scope.planets = resp.data.results.map(planet => {
-    //             planet.residents = planet.residents.map(resident => {
-    //                 var resident = { url: resident };
-    //                 resident.id = resident.url.match(/\d+/)[0];
-    //                 return resident;
-    //             });
-    //             return planet;
-    //         });
+    var myapp = angular.module('myapp', ["ui.router"])
+    myapp.controller("ResidentCtrl", function($scope, $http, $stateParams) {
+        $http.get("http://swapi.co/api/people/" + $stateParams.id + "/?format=json").then(resp => {
+            $scope.character = resp.data;
+        });
+    })
+    myapp.controller("PlanetCtrl", function($scope, $http) {
+        $scope.planets = [];
+        $http.get("http://swapi.co/api/planets/?format=json").then(resp => {
+            $scope.planets = resp.data.results.map(planet => {
+                planet.residents = planet.residents.map(resident => {
+                    var resident = { url: resident };
+                    resident.id = resident.url.match(/\d+/)[0];
+                    return resident;
+                });
+                return planet;
+            });
 
-    //     }).catch(error => console.error(error.status));
-    // });
-    // myapp.config(function($stateProvider, $urlRouterProvider){
+        }).catch(error => console.error(error.status));
+    });
+    myapp.config(function($stateProvider, $urlRouterProvider){
 
-    //   $urlRouterProvider.otherwise("/planets")
+      $urlRouterProvider.otherwise("/planets")
 
-    //   $stateProvider
-    //     .state('planets', {
-    //         url: "/planets",
-    //         templateUrl: "planets.html",
-    //         controller: "PlanetCtrl"
-    //     })
-    //     .state('resident', {
-    //         url: "/resident/:id",
-    //         templateUrl: "resident.html",
-    //         controller: "ResidentCtrl"
-    //     })
-    // })
+      $stateProvider
+        .state('planets', {
+            url: "/planets",
+            templateUrl: "planets.html",
+            controller: "PlanetCtrl"
+        })
+        .state('resident', {
+            url: "/resident/:id",
+            templateUrl: "resident.html",
+            controller: "ResidentCtrl"
+        })
+    })
